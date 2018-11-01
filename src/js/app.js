21790419e1771ff5791bd92713979bd452af0aba
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2018-11-01 22:20:50 +0200

    initial

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+//=include libs/jquery.min.js
+'use strict';
+
+$( document ).ready(() => {
+
+	// your code will be here!
+	const foo = 'Hello';
+	const bar = $('.title');
+	console.log(`${foo}, i'm ES6 template!`, bar);
+
+});
