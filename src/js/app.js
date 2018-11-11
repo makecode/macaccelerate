c4feeabbab883d56711af312df8cdd55ba8faaff
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2018-11-11 19:44:13 +0200

    add some pages

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,24 @@
 'use strict';
 
 $( document ).ready(() => {
+  console.log('run script');
 
-	// your code will be here!
-	const foo = 'Hello';
-	const bar = $('.title');
-	console.log(`${foo}, i'm ES6 template!`, bar);
+  // Change checkbox status on click
+  $('.checkbox').map((index, checkbox) => {
+    $(checkbox).on('click', function(evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+
+      const isChecked = $(checkbox).hasClass('checked');
+
+      if (isChecked) {
+        $(checkbox).removeClass('checked');
+        $(checkbox).find('input:checkbox:first').removeAttr('checked');
+      } else {
+        $(checkbox).addClass('checked');
+        $(checkbox).find('input:checkbox:first').attr('checked', true);
+      }
+    });
+  });
 
 });
